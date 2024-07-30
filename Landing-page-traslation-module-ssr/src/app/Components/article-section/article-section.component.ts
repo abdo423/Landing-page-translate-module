2a5f68968a7649fa5a3bcaac81c79da2a744fd5f
abdo423 <man-body@hotmail.com>
2024-07-30 11:07:26 +0300

    almost finishing article section

--- Landing-page-traslation-module-ssr/src/app/Components/article-section/article-section.component.ts
+++ Landing-page-traslation-module-ssr/src/app/Components/article-section/article-section.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { ArticleCardComponent } from '../article-card/article-card.component';
 @Component({
   selector: 'app-article-section',
   standalone: true,
-  imports: [],
+  imports: [ArticleCardComponent],
   templateUrl: './article-section.component.html',
   styleUrl: './article-section.component.css'
 })
--- Landing-page-traslation-module-ssr/src/app/Components/home/home.component.ts
+++ Landing-page-traslation-module-ssr/src/app/Components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderSectionComponent } from '../header-section/header-section.component';
+import { ArticleSectionComponent } from '../article-section/article-section.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderSectionComponent],
+  imports: [HeaderSectionComponent, ArticleSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
