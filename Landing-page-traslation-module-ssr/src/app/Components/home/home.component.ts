cc20e57119b9f92a6c084e503cafa92538e68ad9
abdo423 <man-body@hotmail.com>
2024-07-31 08:05:21 +0300

    adding subscribe section

--- Landing-page-traslation-module-ssr/src/app/Components/home/home.component.ts
+++ Landing-page-traslation-module-ssr/src/app/Components/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderSectionComponent } from '../header-section/header-section.component';
 import { ArticleSectionComponent } from '../article-section/article-section.component';
 import { AboutSectionComponent } from '../about-section/about-section.component';
+import { SubscribeSectionComponent } from '../subscribe-section/subscribe-section.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderSectionComponent, ArticleSectionComponent, AboutSectionComponent],
+  imports: [HeaderSectionComponent, ArticleSectionComponent, AboutSectionComponent,SubscribeSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
