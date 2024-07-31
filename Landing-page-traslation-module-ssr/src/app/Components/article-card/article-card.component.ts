1b98926cc70f604fbc53d887a2c1ccc4da0a35f0
abdo423 <man-body@hotmail.com>
2024-07-31 07:51:05 +0300

    updating articles section and adding about section

--- Landing-page-traslation-module-ssr/src/app/Components/article-card/article-card.component.ts
+++ Landing-page-traslation-module-ssr/src/app/Components/article-card/article-card.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-article-card',
   standalone: true,
   imports: [],
   templateUrl: './article-card.component.html',
-  styleUrl: './article-card.component.css'
+  styleUrls: ['./article-card.component.css']
 })
 export class ArticleCardComponent {
-   content = 'This is an article card';
+   @Input() content: string = '';
 }
--- Landing-page-traslation-module-ssr/src/app/Components/home/home.component.ts
+++ Landing-page-traslation-module-ssr/src/app/Components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderSectionComponent } from '../header-section/header-section.component';
 import { ArticleSectionComponent } from '../article-section/article-section.component';
+import { AboutSectionComponent } from '../about-section/about-section.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderSectionComponent, ArticleSectionComponent],
+  imports: [HeaderSectionComponent, ArticleSectionComponent, AboutSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
